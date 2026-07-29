59f09f04300329bb998c4e0cbb34d8355702a63b
Victor X <victor.xie@education.nsw.gov.au>
2026-07-29 23:28:48 +0000

    feat: serial monitor experiments complete.

--- 01.serialMonitor/01.serialMonitor.ino
+++ 01.serialMonitor/01.serialMonitor.ino
@@ -30,10 +30,11 @@
 
 // The setup function runs once when you press reset or power the board
 void setup() {
-
+ Serial.begin(115200);                  // Start talking to the computer at 115200 baud
+  Serial.println("Nicker");    // When does this line print?
 }
 
 // The loop function runs over and over again forever
 void loop() {
-
+delay(1000);  // Wait 1 second each cycle
 }
