e65239f4c7261e7ff9e3362576f9ac83945240ac
Victor X <victor.xie@education.nsw.gov.au>
2026-09-22 00:48:25 +0000

    Final Commit

--- 03.buttonsAndDecisions/03.buttonsAndDecisions.ino
+++ 03.buttonsAndDecisions/03.buttonsAndDecisions.ino
@@ -41,9 +41,32 @@ const int BUTTON_PIN = 4;  // Grove Button on D4
 const int LED_PIN = 6;     // Grove LED on D6
 
 void setup() {
-
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(BUTTON_PIN, INPUT);   // this pin will RECEIVE signals
+  Serial.begin(115200);
 }
 
 void loop() {
+  int reading = random(0, 100);
+  Serial.print("Reading: ");
+  Serial.println(reading);
 
+ int zone = reading / 25;    // whole-number division: gives 0, 1, 2 or 3
+
+  switch (zone) {
+    case 0:
+      Serial.println("  zone 0: lowest quarter");
+      break;
+    case 1:
+      Serial.println("  zone 1");
+      break;
+    case 2:
+      Serial.println("  zone 2");
+      break;
+    default:
+      Serial.println("  zone 3: highest quarter");
+      break;
+  }
+
+  delay(1000);
 }
